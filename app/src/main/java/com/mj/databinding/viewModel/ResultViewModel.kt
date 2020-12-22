d5d3a397bbe392ff4ef1e51b65042e358a533c1d
PayMint_MJ <alswhddl10@naver.com>
2020-12-22 11:41:02 +0900

    Port 번호 변경에 따른 수정

--- app/src/main/java/com/mj/databinding/viewModel/ResultViewModel.kt
+++ app/src/main/java/com/mj/databinding/viewModel/ResultViewModel.kt
@@ -1,21 +1,25 @@
 package com.mj.databinding.viewModel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ResultViewModel: ViewModel() {
+class ResultViewModel(): ViewModel() {
     var name: String = ""
     var age: String = ""
     var breed: String = ""
     var color: String = ""
     var character: String = ""
 
-
-    val searchBtnClickInverter = MutableLiveData<Unit>()
+    val searchBtnClickInverter = MutableLiveData<() -> Unit>()
     val goAddClickInverter = MutableLiveData<Unit>()
 
     fun searchBtnClick(){
-        searchBtnClickInverter.value = Unit
+        searchBtnClickInverter.value = {
+
+
+
+        }
     }
 
     fun addBtnClick(){
--- app/src/main/java/com/mj/databinding/viewModel/ResultViewModelFactory.kt
+++ app/src/main/java/com/mj/databinding/viewModel/ResultViewModelFactory.kt
@@ -3,7 +3,8 @@ package com.mj.databinding.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ResultViewModelFactory: ViewModelProvider.Factory{
+class ResultViewModelFactory(): ViewModelProvider.Factory{
+
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return ResultViewModel() as T
     }
